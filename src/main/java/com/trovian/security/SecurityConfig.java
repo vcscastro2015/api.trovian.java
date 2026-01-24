ca4a8f8bcfe8512437a11e6c6a4521478637c0d9
vcscastro2015 <vcscastro@yahoo.com.br>
2026-01-24 10:53:22 -0300

    Austes de permissão

--- src/main/java/com/trovian/security/SecurityConfig.java
+++ src/main/java/com/trovian/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://trovian.trajetto.com.br"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
